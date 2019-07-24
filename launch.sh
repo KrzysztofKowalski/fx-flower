cbab05ddbf131a02612347cee6e19f4b917a311a
Krzysztof Kowalski <krzysztof.kowalski@crossover.com>
2019-07-24 22:52:09 +0200

    x

--- launch.sh
+++ launch.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env dash
 
-find out -type f -iname '*.sh' > commands.sh
+find out -type f -iname '*.sh' | sort -n > commands.sh
 
-parallel --bar -eta -j 32 < commands.sh 
+parallel --progress --bar -eta -j 32 < commands.sh 
