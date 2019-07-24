f9c26ff146cc077166f9c384fa1c02f7c66e23f3
Krzysztof Kowalski <krzysztof.kowalski@crossover.com>
2019-07-24 21:53:41 +0200

    x

--- preparecommands.sh
+++ preparecommands.sh
@@ -4,6 +4,11 @@ for a in `seq 0 100`
 do
 	for b in `seq 0 100`
 	do
+		
+		commanddir="out/commands/${a}/"
+		
+		mkdir -p $commanddir
+		
 		x=$(( $a * 10 + 1 ))
 		y=$(( $b * 10 + 1 ))
 		z=1
@@ -11,7 +16,7 @@ do
 		h=$y
 		j=999
 		increment=1
-		./runner.sh "out/base" $x $y $z $g $h $j $increment > out/commands_${a}_${b}.sh
+		./runner.sh "out/base" $x $y $z $g $h $j $increment > "${commanddir}/${b}.sh"
 	done
 done
 
